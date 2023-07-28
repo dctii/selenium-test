fe518026a125decf295b4367df126a06afb9fd85
dctii <danactolman@gmail.com>
2023-07-28 12:17:48 -0700

    update with email to workflow

--- utils/combine_allure.py
+++ utils/combine_allure.py
@@ -1,8 +1,8 @@
 # utils/combine_allure.py
-# import shutil
-# from datetime import datetime
-# from pytz import timezone
 import os
+import shutil
+from datetime import datetime
+from pytz import timezone
 from allure_combine import combine_allure
 
 
@@ -17,21 +17,21 @@ combine_allure(
     ignore_utf8_errors=False,
 )
 
-# now_utc = datetime.now(timezone('UTC'))
-# now_pdt = now_utc.astimezone(timezone('US/Pacific'))
-# curr_time_pdt = now_pdt.strftime('%Y-%m-%d_%Hh-%Mm')
-#
-# # Rename the generated index.html to single-page.html
-# new_dirname = "single-page"
-# new_filename = f"{curr_time_pdt}-allure-report.html"
-# print("Renaming file...")
-# old_file = os.path.join(allure_report_dir, new_dirname, "complete.html")
-# new_file = os.path.join(allure_report_dir, new_dirname, new_filename)
-#
-# if os.path.exists(old_file):
-#     shutil.move(old_file, new_file)
-# else:
-#     print("old_file path doesn't exist")
-#
-# print("File renamed successfully!")
-# print(f"New allure report filename: {allure_report_dir}/{new_filename}")
+now_utc = datetime.now(timezone('UTC'))
+now_pdt = now_utc.astimezone(timezone('US/Pacific'))
+curr_time_pdt = now_pdt.strftime('%Y-%m-%d_%Hh-%Mm')
+
+# Rename the generated index.html to single-page.html
+new_dirname = "single-page"
+new_filename = f"{curr_time_pdt}-allure-report.html"
+print("Renaming file...")
+old_file = os.path.join(allure_report_dir, new_dirname, "complete.html")
+new_file = os.path.join(allure_report_dir, new_dirname, new_filename)
+
+if os.path.exists(old_file):
+    shutil.move(old_file, new_file)
+else:
+    print("old_file path doesn't exist")
+
+print("File renamed successfully!")
+print(f"New allure report filename: {allure_report_dir}/{new_filename}")
